1a87ad645756b737682ec67a53d25eb1841ffea5
d_alfa <dargis.alfonsas@gmail.com>
2024-01-27 16:39:44 +0200

    Update login_view

--- Project/website/views.py
+++ Project/website/views.py
@@ -6,15 +6,14 @@ from django.contrib.auth import authenticate, login
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
-        # request.POST.get("username")
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('/')
+            return redirect('home')
         else:
             return render(request, 'website/login.html', {'error': 'Invalid login credentials'})
     return render(request, 'website/login.html')
 
-# def home_view(request):
-#     return render(request, 'website/home.html')
+def home_view(request):
+    return render(request, 'website/home.html')
