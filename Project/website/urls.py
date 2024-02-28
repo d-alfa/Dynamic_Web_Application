50ba76a08b5e4cf8732eb899a3165625186df56e
d_alpha <dargis.alfonsas@gmail.com>
2024-02-28 15:50:17 +0200

    Update url patterns

--- Project/website/urls.py
+++ Project/website/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from website import views
 
 urlpatterns = [
-    path("", views.register_view, name="register"),
-    path("registration/", views.register_view, name="register"),
+    path("", views.index_view, name="index"),
+    path("signup/", views.sign_up_view, name="sign_up"),
     path("login/", views.login_view, name="login"),
     path("home/", views.home_view, name="home"),
 ]
