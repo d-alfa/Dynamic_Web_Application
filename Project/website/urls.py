481b0b58d977550b73ef256a13932965ebf996d6
d_alpha <dargis.alfonsas@gmail.com>
2024-02-28 18:38:24 +0200

    Update url patterns

--- Project/website/urls.py
+++ Project/website/urls.py
@@ -5,6 +5,6 @@ from website import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("signup/", views.sign_up_view, name="sign_up"),
-    path("login/", views.login_view, name="login"),
+    path("login/", views.login_view, name="log_in"),
     path("home/", views.home_view, name="home"),
 ]
