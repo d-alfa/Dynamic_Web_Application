d2816a4cfff944d6dc95d6a75ea17e20bb969d78
d_alpha <dargis.alfonsas@gmail.com>
2024-03-10 20:17:15 +0200

    Create test case for index_view

--- Project/website/tests.py
+++ Project/website/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .views import index_view
+
+class Views_Test_Cases(TestCase):
+
+    def test_index_view(self):
+        # Make a GET request to the view using reverse to get the URL
+        response = self.client.get(reverse("index"))
+
+        # Check that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the template 'website/index.html' was used
+        self.assertTemplateUsed(response, "website/index.html")
