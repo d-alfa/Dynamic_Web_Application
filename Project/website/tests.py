f9ee57e338dbdc47516b49e186f59e73c993762b
d_alpha <dargis.alfonsas@gmail.com>
2024-03-20 22:20:47 +0200

    Create log_in_view test cases

--- Project/website/tests.py
+++ Project/website/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.hashers import make_password
 
-from .views import index_view, sign_up_view
 from .models import CustomUser
 
 class Index_View_Test_Cases(TestCase):
@@ -36,7 +36,7 @@ class Sign_Up_View_Test_Case(TestCase):
         response = self.client.get(reverse("sign_up"))
         self.assertTemplateUsed(response,"website/sign_up.html")
 
-    # Check if passwords do not match && redirect to sign_up page
+    # Check if passwords do not match
     def test_sign_up_password_match(self): 
         response = self.client.post(reverse('sign_up'), {
             'username': 'test_user',
@@ -67,3 +67,45 @@ class Sign_Up_View_Test_Case(TestCase):
         })
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("log_in"))
+
+class Log_in_View_Test_Case(TestCase):
+
+    # Test setup
+    def setUp(self):
+        self.client = Client()
+
+    # Check if status code is 200
+    def test_log_in_status_code(self): 
+        response = self.client.get(reverse("log_in"))
+        self.assertEqual(response.status_code, 200)
+
+    # Check if template "log_in.html" was used  
+    def test_log_in_template_used(self): 
+        response = self.client.get(reverse("log_in"))
+        self.assertTemplateUsed(response,"website/log_in.html")
+
+    # Check for invalid credentials
+    def test_log_in_for_invalid_credentials(self):
+        CustomUser.objects.create(username="test_user", password="test_password")
+        response = self.client.post(reverse('log_in'), {
+            'username': 'test_user',
+            'password': 'test_password123'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Invalid log in credentials")
+        
+    # Check for user authentication
+    def test_log_in_user_authentication(self):
+        CustomUser.objects.create(username="test_user", password=make_password("test_password"))
+        login_successfull = self.client.login(username="test_user", password="test_password")
+        self.assertTrue(login_successfull)
+
+    # Check for redirection to home page
+    def test_log_in_redirection_to_home(self):
+        CustomUser.objects.create(username="test_user", password=make_password("test_password"))
+        response = self.client.post(reverse('log_in'), {
+            'username': 'test_user',
+            'password': 'test_password'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("home")) 
