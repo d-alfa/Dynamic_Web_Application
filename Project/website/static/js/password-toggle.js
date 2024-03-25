38290be0cdf3c40d283f1ee3ba27e08ff28b2888
d_alpha <dargis.alfonsas@gmail.com>
2024-03-25 04:52:03 +0200

    Create password-toggle.js

---
+++ Project/website/static/js/password-toggle.js
@@ -0,0 +1,15 @@
+
+function togglePasswordVisibility() {
+    var passwordField = document.getElementById('password');
+    var icon = document.getElementById('togglePassword');
+    
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordField.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
